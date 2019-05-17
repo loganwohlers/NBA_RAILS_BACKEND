e72e83cf1de66a6807bc36400dfa24fcb9b15115
Logan Wohlers <lawohlers@gmail.com>
2019-05-17 12:50:58 -0700

    re-added join query w/ take

--- lib/query.rb
+++ lib/query.rb
@@ -3,13 +3,9 @@ require_relative '../config/environment.rb'
 lebron=Player.find_by(name: "LeBron James")
 season=Season.first
 
-
 avg=lebron.get_player_season(2018)
 
 p avg
 puts
 
-p games
-p games.length
-puts
 
