4c042bd57f92244d553b95f5735d68d1fd6e8a03
Logan Wohlers <lawohlers@gmail.com>
2019-05-20 11:04:19 -0700

    fixed up serializer more

--- app/serializers/player_season_serializer.rb
+++ app/serializers/player_season_serializer.rb
@@ -1,8 +1,15 @@
 class PlayerSeasonSerializer < ActiveModel::Serializer
-    attributes :id, :player, :season, :team
+    attributes :id, :player, :season, :team, :age, :mp_per_g, :fg_per_g, :fga_per_g, :fg_pct,
+    :fg3_per_g,:fg3a_per_g, :fg3_pct, :efg_pct, :ft_per_g, :fta_per_g, :ft_pct, :orb_per_g,
+    :drb_per_g, :ast_per_g, :stl_per_g, :blk_per_g, :tov_per_g, :pts_per_g
     belongs_to :player
     belongs_to :team
     belongs_to :season
-    
+#     def owner
+#     {owner_id: self.object.person.id, 
+#      owner_name: self.object.person.name}
+#   end 
+
     # has_many :game_lines    
 end
+
