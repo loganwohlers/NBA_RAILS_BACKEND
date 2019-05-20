01ba3f440235f4bb81bdec601b4674fcff7d9c59
Logan Wohlers <lawohlers@gmail.com>
2019-05-20 15:26:34 -0700

    big fix on home/away team in serializer

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,9 +1,28 @@
 class GameSerializer < ActiveModel::Serializer
 
     attributes :id, :code, :date, :start_time, :home_pts, :away_pts
+    # , :winner
     belongs_to :season
     belongs_to :home_team, :class_name => "TeamSeason"
     belongs_to :away_team, :class_name => "TeamSeason"
+
+    def season
+        {year: self.object.season.year}
+    end 
+
+    def home_team
+        {name: self.object.home_team.team.name,
+        code: self.object.home_team.team.code }
+    end
+
+    def away_team
+        {name: self.object.away_team.team.name,
+        code: self.object.away_team.team.code }
+    end
+
+    # def winner
+    #     {winner: self.winner}
+    # end
     # has_many :game_lines
 
 end
