6b49084b23051309589deb89d033be44c9397bf5
Logan Wohlers <lawohlers@gmail.com>
2019-05-21 12:33:14 -0700

    fixed serializer for gameline

--- app/serializers/game_line_serializer.rb
+++ app/serializers/game_line_serializer.rb
@@ -1,11 +1,15 @@
 class GameLineSerializer < ActiveModel::Serializer
 
-    attributes :id, :mp, :fg, :fga, :fg_pct, :fg3, :ft, :fta, :ft_pct, :orb, :drb, :trb, :ast, :stl, :blk, :tov, :pts, :plus_minus, :dnp
+    attributes :id, :mp, :fg, :fga, :fg_pct, :fg3, :fg3a, :fg3_pct, :ft, :fta, :ft_pct, :orb, :drb, :trb, :ast, :stl, :blk, :tov, :pf, :pts, :plus_minus, :dnp
+
     belongs_to :game
     belongs_to :player_season
 
+
     def player_season
-        {player: self.object.player_season.player.name}
-    end     
+        {player: self.object.player_season.player.name,
+        team: self.object.player_season.team.name}
+    end    
+   
 
 end
