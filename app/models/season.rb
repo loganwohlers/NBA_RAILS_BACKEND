43fd92a686a6549fceda5f08e6b28d6598240c68
Logan Wohlers <lawohlers@gmail.com>
2019-06-05 20:11:27 -0700

    fixed new assoc between season and player_seasons

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,5 +1,9 @@
 class Season < ApplicationRecord
     has_many :games
     has_many :team_seasons
-    has_many :player_seasons
+    has_many :player_seasons, through: :team_seasons
+    
+
+
+
 end
